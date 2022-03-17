50b7bf538de58029a6fbed275a61930faf3d79ad
BHC-03 <7meeed999@gmail.com>
2022-03-17 16:31:41 +0200

    added a new custom hook

---
+++ src/components/Test/useForm.js
@@ -0,0 +1,28 @@
+import {useState,useEffect} from 'react';
+
+const useForm = (atom)=>{
+    const [errors,setErrors] = useState({
+        name:'',
+        email:'',
+        password:''
+    });
+    useEffect(()=>{
+        if(!atom.email){
+            setErrors(olderrors=>({...olderrors,email:'email is required'}))
+        }else if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(atom.email))){
+            setErrors(olderrors=>({...olderrors,email:'email is Invalid'}))
+        }else if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(atom.email)){
+            setErrors(olderrors=>({...olderrors,email:''}))
+        }
+    
+        if(!atom.password){
+            setErrors(olderrors=>({...olderrors,password:'password is required'}))
+        }else if(atom.password){
+            setErrors(olderrors=>({...olderrors,password:''}))
+        }
+    },[atom])
+
+    return(errors)
+}
+
+export default useForm;
--- src/components/login.js
+++ src/components/login.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React , {useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope,faLock,faUser} from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import {userInfoAtom,gettingUserinfoAtom} from '../RecoilStuff/index';
 import { useSetRecoilState,useRecoilState } from "recoil";
-
+import useForm from "./Test/useForm";
 
 function LoginForm(){
     const [userInfo,setUserInfo] = useRecoilState(gettingUserinfoAtom);
     const setSignedUserInfo = useSetRecoilState(userInfoAtom);
+    const errors = useForm(userInfo);
+    const [isGood,setIsGood] = useState(true);
     function loginSubmitHandler(){
+        if(errors){
+            return setIsGood(false);
+        }
         axios.post('http://127.0.0.1:8000/users/login/',
         {
             email:userInfo.email,
@@ -18,6 +23,7 @@ function LoginForm(){
             name:userInfo.username
         }).then(
             data=>{
+                
                 setSignedUserInfo(data.data);
                 window.sessionStorage.setItem('userInfo',JSON.stringify(data.data))
             }
@@ -34,19 +40,24 @@ function LoginForm(){
     }
     return (
         <div className="loginForm">
-           <div className="registerInputContainer">
+           <div className="loginInputsContainer">
                     <div className="textInputContainer">
                         <FontAwesomeIcon icon={faEnvelope} className="inputIcon emailIcon" />
                         <input type="text" value={userInfo.email} onChange={emailHandler} className="textInput emailInput" />
+                        
                     </div>
+                    {!isGood && errors.email && (
+                            <p className="errorMessage">{errors.email}</p>
+                        )}
                     <div className="textInputContainer">
                         <FontAwesomeIcon icon={faLock} className="inputIcon passwordIcon" />
                         <input value={userInfo.password} onChange={passwordHandler} type="password" className="textInput passwordInput"/>
+                        
                     </div>
-                    <div className="textInputContainer">
-                        <FontAwesomeIcon icon={faUser} className="inputIcon usernameIcon" />
-                        <input  value={userInfo.username} onChange={usernameHandler} type="text"className="textInput usernameInput" />
-                    </div>
+                    {!isGood && errors.password && (
+                            <p className="errorMessage">{errors.password}</p>
+                        )}
+                    
                 </div>
             <Link to={'/mainpage/membership'}><p onClick={loginSubmitHandler} className="button loginSubmit">
                 Login
